7936151437ab3b3ece3bac1f2b8f6161f015b02f
Harrison Young <jaaina2010@hotmail.com>
2021-08-01 18:26:38 -0500

    Added entry widgets to input form

--- jmain.py
+++ jmain.py
@@ -1,17 +1,14 @@
 from tkinter import *
-from typing import Sized
-from jpacks.income import Income
-from jpacks.dbconnect import *
+# from typing import Sized
 
 
-#db = connect_to_db()
-#mycursor = db.cursor()
 #mycursor.execute("CREATE TABLE CompletedJobs (jobid int PRIMARY KEY AUTO_INCREMENT, jobdate DATE, client VARCHAR(50),"
 #                "buyer VARCHAR(50), platform VARCHAR(25), pay DECIMAL(6,2))")#mycursor.execute("DESCRIBE CompletedJobs")
-#for x in mycursor:
-#    print(x)
-#income = Income("7/12/2021", "Iberia Bank", "Core Tech", "WM", "234.00")
-
+# for x in mycursor:
+#     print(x)
+# income = Income("7/12/2021", "Iberia Bank", "Core Tech", "WM", "234.00")
+# income.save_records()
+# income.get_records()
 
 #Define window dimensions and calculate center of screen position
 ja = Tk()
@@ -35,19 +32,30 @@ input_title_frame.grid(row=0, column=0)
 #Display input fields headers
 input_headers_frame = Frame(ja)
 date_header = Label(input_headers_frame, text="Date",fg="blue")
-date_header.grid(row=0, column=0, padx=(0,100))
+date_header.grid(row=0, column=0, padx=(50,100))
 client_header = Label(input_headers_frame, text="Client",fg="blue")
 client_header.grid(row=0, column=1, padx=(0,0))
 buyer_header = Label(input_headers_frame, text="Buyer", fg="blue")
-buyer_header.grid(row=0, column=2, padx=(100,50))
+buyer_header.grid(row=0, column=2, padx=(115,5))
 platform_header = Label(input_headers_frame, text="Platform", fg="blue")
-platform_header.grid(row=0, column=3, padx=(50,100))
+platform_header.grid(row=0, column=3, padx=(100,100))
 pay_header = Label(input_headers_frame, text="Pay",fg="blue")
-pay_header.grid(row=0, column=4, padx=(0,80))
+pay_header.grid(row=0, column=4, padx=(20,80))
 input_headers_frame.grid(row=1, column=0)
 
 #display entry boxes
-entry_boxes_frame= Frame(ja, height=30, width=window_width- 10, bg="blue", relief=SOLID)
+entry_boxes_frame= Frame(ja, height=30, width=window_width- 10, relief=SOLID)
+date_entry = Entry(entry_boxes_frame)
+date_entry.grid(row=0, column=0)
+client_entry = Entry(entry_boxes_frame)
+client_entry.grid(row=0, column=1, padx=(20,0))
+buyer_entry = Entry(entry_boxes_frame)
+buyer_entry.grid(row=0, column=2, padx=(20,0))
+platform_entry = Entry(entry_boxes_frame)
+platform_entry.grid(row=0, column=3, padx=(25,0))
+pay_entry = Entry(entry_boxes_frame)
+pay_entry.grid(row=0, column=4, padx=(25,25))
+
 entry_boxes_frame.grid(row=2,column=0)
 
 ja.mainloop()
